dfc32184fbcbed32ab0b470edf4675fdb6f005e2
Alessandro Sales <contato@alessandrosales.com.br>
2016-11-26 00:41:24 -0300

    Adicionei uma classe que trata dos tiros no game e dei uma refatorada no codigo

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
@@ -15,10 +16,18 @@ def run_game():
 
 	ship = Ship(ai_settings, screen)
 
+	bullets = Group()
+
 	while True:
 
-		gf.check_events(ship)
+		gf.check_events(ai_settings, screen, ship, bullets)
 		ship.update()
-		gf.update_screen(ai_settings, screen, ship)
+		bullets.update()
+
+		for bullet in bullets.copy():
+			if bullet.rect.bottom <= 0:
+				bullets.remove(bullet)
+
+		gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -1,32 +1,43 @@
 import sys
 import pygame
+from bullet import Bullet
 
-def check_events(ship):
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
+	if event.key == pygame.K_RIGHT:
+		ship.moving_right = True
+	elif event.key == pygame.K_LEFT:
+		ship.moving_left = True
+	elif event.key == pygame.K_SPACE:
+		new_bullet = Bullet(ai_settings, screen, ship)
+		bullets.add(new_bullet)
 
+def check_keyup_events(event, ship):
+	if event.key == pygame.K_RIGHT:
+		ship.moving_right = False
+	elif event.key == pygame.K_LEFT:
+		ship.moving_left = False
+
+def check_events(ai_settings, screen, ship, bullets):
 	for event in pygame.event.get():
 
 		if event.type == pygame.QUIT:
 			sys.exit()
 
 		elif event.type == pygame.KEYDOWN:
-
-			if event.key == pygame.K_RIGHT:
-				ship.moving_right = True
-
-			if event.key == pygame.K_LEFT:
-				ship.moving_left = True
+			check_keydown_events(event, ai_settings, screen, ship, bullets)
 
 		elif event.type == pygame.KEYUP:
+			check_keyup_events(event, ship)
 
-			if event.key == pygame.K_RIGHT:
-				ship.moving_right = False
 
-			if event.key == pygame.K_LEFT:
-				ship.moving_left = False
 
-def update_screen(ia_settings, screen, ship):
+def update_screen(ia_settings, screen, ship, bullets):
 
 	screen.fill(ia_settings.bg_color)
+
+	for bullet in bullets.sprites():
+		bullet.draw_bullet();
+
 	ship.blitme()
 
 	pygame.display.flip()
