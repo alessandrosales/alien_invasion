003239cd48ded788774a455e5700a8cf184b5c1f
Alessandro Sales <contato@alessandrosales.com.br>
2016-11-27 13:52:52 -0300

    refatoração do código, criação de função para disparar os tiros e limite no número de tiros

--- game_functions.py
+++ game_functions.py
@@ -8,8 +8,7 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
 	elif event.key == pygame.K_LEFT:
 		ship.moving_left = True
 	elif event.key == pygame.K_SPACE:
-		new_bullet = Bullet(ai_settings, screen, ship)
-		bullets.add(new_bullet)
+		fire_bullet(ai_settings, screen, ship, bullets)
 
 def check_keyup_events(event, ship):
 	if event.key == pygame.K_RIGHT:
@@ -29,10 +28,20 @@ def check_events(ai_settings, screen, ship, bullets):
 		elif event.type == pygame.KEYUP:
 			check_keyup_events(event, ship)
 
+def fire_bullet(ia_settings, screen, ship, bullets):
+	if len(bullets) < ia_settings.bullets_allowed:
+		new_bullet = Bullet(ia_settings, screen, ship)
+		bullets.add(new_bullet)
 
+def update_bullets(bullets):
+	bullets.update()
+
+	for bullet in bullets.copy():
+		if bullet.rect.bottom <= 0:
+			bullets.remove(bullet)
+	print(len(bullets))
 
 def update_screen(ia_settings, screen, ship, bullets):
-
 	screen.fill(ia_settings.bg_color)
 
 	for bullet in bullets.sprites():
